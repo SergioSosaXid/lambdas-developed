200d6dfaead3e3d7ff3e58c6dbd37751c60006b1
Sergio Sosa <sergio.sosa@apscreativas.com>
2021-05-13 10:11:35 -0500

    Se actualizo el authorizer y layers

--- src/lambdas/authorizer/handler.ts
+++ src/lambdas/authorizer/handler.ts
@@ -2,6 +2,10 @@ import { PolicyIamManager } from "../../common/helpers/PolicyIamManager";
 import { AuthorizerManager } from "../authorizer/manager"
 import  { isObjEmpty } from "../../common/helpers/objectManager"
 
+import { ResponseManager} from '../../common/helpers/responseManager'
+
+
+const responseManager = new ResponseManager()
 export async  function authorizer(event:any, context:any, callback:any) {
 
     const authorizerMagager = new AuthorizerManager()
@@ -20,8 +24,7 @@ export async  function authorizer(event:any, context:any, callback:any) {
             callback(null, policyIamManager.getPolicy('user', "Deny", event.methodArn));
             break;
         case 'unauthorized':
-            callback("Unauthorized");   // Return a 401 Unauthorized response
-            break;
+            return responseManager.handleError("Sin autorización")
         default:
             callback("Error: Invalid token"); // Return a 500 Invalid token response
     } 
--- src/lambdas/authorizer/manager.ts
+++ src/lambdas/authorizer/manager.ts
@@ -9,14 +9,20 @@ const jwkTopem = require("jwk-to-pem");
 
 export class AuthorizerManager {
 
-    protected jwk:any = {
+   /* protected jwk:any = {
         "alg": "RS256",
         "e": "AQAB",
         "kid": "ZaE1z9Q+nYqOjYPnOElc6TQkfXhDhPcskAiskY0/hQk=",
         "kty": "RSA",
         "n": "wxHu-bZFyYgSEyP9HNMfmabJ2JtzWo8mtSv46DPnAOD4ApllgsVNNa8aF6WL1ekbGLAWT1IqCIkCDYY0g48slaLuKsm0yB9ILIhof_5zyDZI-Z-PUFv-6fsy7O4VTjvGdv4pSedem_cuJnQk42ZEA52fB2JdwO0Kzn3N3QKBwMvpNuXpfmiowmnnudWHMQAPwy5qn4fO20MHscGTXwKPNdUX3xi5Q_d0cUZBhR26x5FiL9KLTXuh9oHN_AIJ_wMpVXR2ihal1zXoyvlSIrrW-XKAqREv_XGy-xa1Bo7SA0OxNblx2Zo94zYWZPRIjgLXF4QjmPCqht1gxqu0bAGKmw",
         "use": "sig"
-    }
+    } */
+    protected jwk:any = {"alg":"RS256",
+    "e":"AQAB",
+    "kid":"IaWurm5o/VZx6EMFR7tHsTQx3gNf103e7Kp53415sDE=",
+    "kty":"RSA",
+    "n":"53k2kWn3Q1E3mTd-88qAT54zr0EBssfhAjUCALPJQIUb5JlCiwAkuSQ4SBIwGtaLtnQaBN_w25oWU0kducdIszjF6cReaLzPSHkQSoWBuMz11p7xVA8XrMMZQhSmwhKgsp8Wgzp4elFWh-Oks9klVTTrlUbz9O5dwbVzw0Y1GSSfH7INOhkaFF6alkk7ETGrMV7JTbhGnZ6kyu-uw9D-jw8oOj3f4pkFlY77nnG6Y1zEU-JT5sk14j0YwdaEGqxsVSXq74_k0gtR0ei9OQ_kkeVdyaKsn9Uq18JIRjEyxgfV2RyOGNlPe9lNDASnj4cDOwcYNVZ3ac3sCGtUCy9-Aw",
+    "use":"sig"}
 
     /**
      * @return {Promise<>}
